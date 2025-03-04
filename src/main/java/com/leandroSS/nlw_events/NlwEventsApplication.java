b34d5f78844a66a64f95910155e0aec2cf8f9f53
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2025-03-04 14:51:41 -0300

    projeto zerado

---
+++ src/main/java/com/leandroSS/nlw_events/NlwEventsApplication.java
@@ -0,0 +1,13 @@
+package com.leandroSS.nlw_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroSS/nlw_events/NlwEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroSS.nlw_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
